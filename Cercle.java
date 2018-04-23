2cdea6a84f018df46607d7318bd5d60827f66a2a
Joelshowkats <37113210+Joelshowkats@users.noreply.github.com>
2018-04-23 16:44:59 +0200

    Update Cercle.java

--- Cercle.java
+++ Cercle.java
@@ -14,17 +14,38 @@ public class Cercle {
 		x=0;
 		y=0;
 	}
+	
 	Cercle (Cercle c)
 	{
 		x=c.X();
 		y=c.Y();
 	}
 	
-	@Override
-    public String toString()
+	public float getX()
 	{
-        return "x: "+x+ " y: "+y;
-    }
+		return x;
+	}
+	
+	public void setX()
+	{
+		this.x=xx;
+	}
+	
+	public float getY()
+	{
+		return y;
+	}
+	
+	public void setY()
+	{
+		this.x=xx;
+	}
+	
+	@Override
+    	public String toString()
+	{
+        	return "x: "+x+ " y: "+y;
+    	}
 	
 	@Override
 	public Boolean equals(Cercle c)
